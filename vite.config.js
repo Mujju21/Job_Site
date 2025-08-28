638c1abef3a59b8766353d92d133cd402bce8670
unknown <mujamillshaikh@gmail.com>
2025-08-28 16:07:47 +0530

    save work before rebase

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins : [react()],
+  base:'/Job_Site',
   server : {
     port:5000,
     proxy:{
